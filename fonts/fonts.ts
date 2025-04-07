542a13d7bf718c999bae1cd65f013f2b7747396a
Federico Herrera <fede.juan.herrera@gmail.com>
2025-04-07 10:11:21 -0300

    fixing font

--- fonts/fonts.ts
+++ fonts/fonts.ts
@@ -1,6 +1,4 @@
 
-import { Gloria_Hallelujah, Patrick_Hand, Architects_Daughter } from 'next/font/google'
+import { Architects_Daughter } from 'next/font/google'
 
-export const gloria = Gloria_Hallelujah({ subsets: ['latin'], weight: '400' });
-export const patrick = Patrick_Hand({ subsets: ['latin'], weight: '400' });
 export const architects = Architects_Daughter({ subsets: ['latin'], weight: '400' });
