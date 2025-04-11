938614fe9323dbf2089632ee6d0c001689e481ab
Federico Herrera <fede.juan.herrera@gmail.com>
2025-04-11 16:57:44 -0300

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="https://tyvgqomjokwdofxjtxbc.supabase.co/storage/v1/object/sign/logos/institution_icon_144269.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsb2dvcy9pbnN0aXR1dGlvbl9pY29uXzE0NDI2OS53ZWJwIiwiaWF0IjoxNzQ0NDAxNDMxLCJleHAiOjE5MDIwODE0MzF9.jc3_2CuavbIXUCDxZLwO3ZshlMDhZ-iETT8Jkv7RFVE" />
+      </head>
       <body>
         <main>{children}</main>
         <Toaster />
