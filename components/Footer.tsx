2599f6a6c9b608026cc18ffe730a7131e1b4655e
Federico Herrera <fede.juan.herrera@gmail.com>
2025-04-11 12:41:51 -0300

    changing logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import { architects } from "@/fonts/fonts"
-import { PiSunHorizon } from "react-icons/pi";
 import Image from "next/image";
 import asneedLogo from '@/public/asneedlogo.png'
 
@@ -7,7 +6,6 @@ export const Footer = () => {
     return (
         <footer className="bg-[url('/bg-footer2.png')] bg-cover min-h-[50vh]">
             <div className="flex flex-col items-center gap-2 mt-10 py-20">
-                <PiSunHorizon size={60} className="text-yellow-500" />
                 <h1 className={`font-extralight text-8xl ${architects.className}`}>RIE</h1>
                 <p className="text-zinc-600 text-lg">Red Integral Educativa</p>
             </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import { scrollToSection } from "@/utils/functions";
-import { PiSunHorizon } from "react-icons/pi";
-
+import { architects } from "@/fonts/fonts";
 
 export const Header = () => {
     return (
         <div className="flex md:flex-row flex-col justify-between items-center md:px-20 md:gap-0 gap-5 py-5">
             <div>
-                <PiSunHorizon className="text-yellow-500 md:text-5xl text-6xl" />
+                <h1 className={`${architects.className} text-4xl`}>RIE</h1>
             </div>
             <div className="flex items-center md:text-xl md:gap-10 gap-7 text-zinc-700 cursor-pointer">
                 <p className="" onClick={() => scrollToSection('proposal')}>La propuesta</p>
